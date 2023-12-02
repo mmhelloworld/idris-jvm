7b3ee6a0efc189e8bc730ecd9f4d6b83c1b41b84
jneira <atreyu.bbb@gmail.com>
2023-12-02 16:38:49 +0100

    Some more cases

--- idris-jvm-runtime/src/test/java/io/github/mmhelloworld/idrisjvm/runtime/PathsTest.java
+++ idris-jvm-runtime/src/test/java/io/github/mmhelloworld/idrisjvm/runtime/PathsTest.java
@@ -15,11 +15,16 @@ public class PathsTest {
     String cwd = Directories.getWorkingDir();
     // File system separator
     String sep = System.getProperty("file.separator");
+
+    assertThat(Paths.createPath(null).toString()).isEqualTo(cwd);
+    assertThat(Paths.createPath("").toString()).isEqualTo(cwd);
     assertThat(Paths.createPath(cwd).toString()).isEqualTo(cwd);
+
     assertThat(Paths.createPath("a").toString()).isEqualTo(
       cwd + sep + "a");
     assertThat(Paths.createPath("a" + sep +"b").toString()).isEqualTo(
       cwd + sep + "a" + sep + "b");
+
     assertThat(Paths.createPath(".." + sep + "a").toString()).isEqualTo(
       Paths.createPath(cwd).getParent() + sep + "a");
     assertThat(Paths.createPath("." + sep + "a").toString()).isEqualTo(

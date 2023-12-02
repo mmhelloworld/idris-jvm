e2b7c7a0be6ba8f49c360bb83514cbbf83acc5c2
jneira <atreyu.bbb@gmail.com>
2023-12-02 16:32:05 +0100

    Use java nio api to normalize paths

--- idris-jvm-runtime/src/main/java/io/github/mmhelloworld/idrisjvm/runtime/Paths.java
+++ idris-jvm-runtime/src/main/java/io/github/mmhelloworld/idrisjvm/runtime/Paths.java
@@ -11,29 +11,14 @@ public final class Paths {
     }
 
     private static Path createPath(String pathStr, String workingDir) {
-        if (pathStr.isEmpty()) {
-            return java.nio.file.Paths.get(workingDir);
+
+        Path cwd = java.nio.file.Paths.get(workingDir);
+
+        if (pathStr == null || pathStr.isEmpty()) {
+            return cwd;
         }
 
         Path path = java.nio.file.Paths.get(pathStr);
-        final Path resolvedPath;
-        if (path.isAbsolute()) {
-            resolvedPath = path;
-        } else {
-            if (pathStr.startsWith("../")) {
-                resolvedPath = createPath(pathStr.substring("../".length()),
-                    java.nio.file.Paths.get(workingDir).getParent().toString());
-            } else if (pathStr.startsWith("..")) {
-                resolvedPath = createPath(pathStr.substring("..".length()),
-                    java.nio.file.Paths.get(workingDir).getParent().toString());
-            } else if (pathStr.startsWith("./")) {
-                resolvedPath = createPath(pathStr.substring("./".length()), workingDir);
-            } else if (pathStr.startsWith(".")) {
-                resolvedPath = createPath(pathStr.substring(".".length()), workingDir);
-            } else {
-                resolvedPath = java.nio.file.Paths.get(workingDir, pathStr);
-            }
-        }
-        return resolvedPath;
+        return cwd.resolve(path).normalize();
     }
 }
---
+++ idris-jvm-runtime/src/test/java/io/github/mmhelloworld/idrisjvm/runtime/PathsTest.java
@@ -0,0 +1,28 @@
+package io.github.mmhelloworld.idrisjvm.runtime;
+
+import io.github.mmhelloworld.idrisjvm.runtime.Paths;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.nio.file.Path;
+
+public class PathsTest {
+
+  @Test
+  void test() {
+    String cwd = Directories.getWorkingDir();
+    // File system separator
+    String sep = System.getProperty("file.separator");
+    assertThat(Paths.createPath(cwd).toString()).isEqualTo(cwd);
+    assertThat(Paths.createPath("a").toString()).isEqualTo(
+      cwd + sep + "a");
+    assertThat(Paths.createPath("a" + sep +"b").toString()).isEqualTo(
+      cwd + sep + "a" + sep + "b");
+    assertThat(Paths.createPath(".." + sep + "a").toString()).isEqualTo(
+      Paths.createPath(cwd).getParent() + sep + "a");
+    assertThat(Paths.createPath("." + sep + "a").toString()).isEqualTo(
+      Paths.createPath(cwd) + sep + "a");
+  }
+}

b179d1e24e3e672e01d3fbb86f75f3cb43bca9e8
Marimuthu Madasamy <mmhelloworld@gmail.com>
2024-02-08 19:33:19 -0500

    Generate executable for windows and posix, don't print stacktrace

--- idris-jvm-runtime/src/main/java/io/github/mmhelloworld/idrisjvm/runtime/ChannelIo.java
+++ idris-jvm-runtime/src/main/java/io/github/mmhelloworld/idrisjvm/runtime/ChannelIo.java
@@ -253,7 +253,6 @@ public final class ChannelIo implements ReadableByteChannel, WritableByteChannel
         } else if (currentException instanceof FileAlreadyExistsException) {
             return EXISTING_FILE_ERROR_CODE;
         } else {
-            currentException.printStackTrace();
             return GENERAL_ERROR_CODE;
         }
     }

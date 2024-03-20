dea154535655160f9ee361b384103707bd5e3b7e
Marimuthu Madasamy <mmhelloworld@gmail.com>
2024-03-20 19:54:36 -0400

    Add placeholder class to fix missing javadoc error during release

---
+++ idris-jvm-compiler/src/main/java/io/github/mmhelloworld/idrisjvm/compiler/Placeholder.java
@@ -0,0 +1,13 @@
+package io.github.mmhelloworld.idrisjvm.compiler;
+
+/*
+ * This is to satisfy maven-javadoc-plugin complaining about javadoc jar during release:
+ *  Error: [ERROR] Nexus Staging Rules Failure Report
+ *  Error: [ERROR] ==================================
+ *  Error: [ERROR]
+ *  Error: [ERROR] Repository "iogithub***-1013" failures
+ *  Error: [ERROR]   Rule "javadoc-staging" failures
+ *  Error: [ERROR]     * Missing: no javadoc jar found in folder
+ */
+public class Placeholder {
+}

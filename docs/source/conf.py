5fcd60fe8f1b6dadc02ebef0991ac6857adcd816
Marimuthu Madasamy <mmhelloworld@gmail.com>
2024-07-14 23:31:32 -0400

    #173 Fix docs build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,5 +22,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_theme_options = {'navigation_depth': 5}
-html_static_path = ['_static']
+html_theme_options = {'navigation_depth': 5, 'display_version': True}
